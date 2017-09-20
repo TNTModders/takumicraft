4869824617b30e3616b8875da504769c4560a74d
Tom Kate <https://tomkate@bitbucket.org/tntmodders/project_fassion.git>
2017-09-20 22:23:45 +0900

    [34]微匠の追加

---
+++ src/main/java/com/tntmodders/takumi/entity/mobs/EntityHappinessCreeper.java
@@ -0,0 +1,64 @@
+package com.tntmodders.takumi.entity.mobs;
+
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.ExplosionEvent;
+
+public class EntityHappinessCreeper extends EntityTakumiAbstractCreeper {
+    public EntityHappinessCreeper(World worldIn) {
+        super(worldIn);
+    }
+
+    @Override
+    public void takumiExplode() {
+    }
+
+    @Override
+    public boolean takumiExplodeEvent(ExplosionEvent.Detonate event) {
+        for (Entity entity : event.getAffectedEntities()) {
+            if (entity instanceof EntityLivingBase) {
+                ((EntityLivingBase) entity).setHealth(((EntityLivingBase) entity).getMaxHealth());
+                ((EntityLivingBase) entity).clearActivePotions();
+            }
+        }
+        event.getAffectedEntities().clear();
+        return true;
+    }
+
+    @Override
+    public EnumTakumiRank takumiRank() {
+        return EnumTakumiRank.LOW;
+    }
+
+    @Override
+    public EnumTakumiType takumiType() {
+        return EnumTakumiType.GROUND;
+    }
+
+    @Override
+    public int getExplosionPower() {
+        return 6;
+    }
+
+    @Override
+    public int getSecondaryColor() {
+        return 16711935;
+    }
+
+    @Override
+    public boolean isCustomSpawn() {
+        return false;
+    }
+
+    @Override
+    public String getRegisterName() {
+        return "happinesscreeper";
+    }
+
+    @Override
+    public int getRegisterID() {
+        return 25;
+    }
+}
