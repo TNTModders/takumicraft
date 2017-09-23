242e9ddd0e8b2b8cad6c489ea849d91a0f552a06
Tom Kate <https://tomkate@bitbucket.org/tntmodders/project_fassion.git>
2017-09-23 22:27:24 +0900

    [43]ダイヤモンド匠の追加

---
+++ src/main/java/com/tntmodders/takumi/entity/mobs/EntityDiamondCreeper.java
@@ -0,0 +1,72 @@
+package com.tntmodders.takumi.entity.mobs;
+
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.util.DamageSource;
+import net.minecraft.world.World;
+
+public class EntityDiamondCreeper extends EntityTakumiAbstractCreeper {
+    public EntityDiamondCreeper(World worldIn) {
+        super(worldIn);
+    }
+
+    protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).setBaseValue(100000);
+        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(128);
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(40);
+    }
+
+    public void onDeath(DamageSource source) {
+        if (!this.world.isRemote) {
+            this.dropItem(Item.getItemFromBlock(Blocks.DIAMOND_BLOCK), 1);
+        }
+        super.onDeath(source);
+    }
+
+    @Override
+    public void takumiExplode() {
+    }
+
+    @Override
+    public EnumTakumiRank takumiRank() {
+        return EnumTakumiRank.MID;
+    }
+
+    @Override
+    public EnumTakumiType takumiType() {
+        return EnumTakumiType.WATER;
+    }
+
+    @Override
+    public int getExplosionPower() {
+        return 5;
+    }
+
+    @Override
+    public int getSecondaryColor() {
+        return 0x0000ff;
+    }
+
+    @Override
+    public int getPrimaryColor() {
+        return 0x9090a0;
+    }
+
+    @Override
+    public boolean isCustomSpawn() {
+        return false;
+    }
+
+    @Override
+    public String getRegisterName() {
+        return "diamondcreeper";
+    }
+
+    @Override
+    public int getRegisterID() {
+        return 210;
+    }
+}
