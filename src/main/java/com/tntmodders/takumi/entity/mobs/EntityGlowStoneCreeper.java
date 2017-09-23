ce4554ade2ee69ee09778c019575de30a726ab74
Tom Kate <https://tomkate@bitbucket.org/tntmodders/project_fassion.git>
2017-09-23 17:25:41 +0900

    [37]グロウストーン匠の追加

---
+++ src/main/java/com/tntmodders/takumi/entity/mobs/EntityGlowStoneCreeper.java
@@ -0,0 +1,89 @@
+package com.tntmodders.takumi.entity.mobs;
+
+import com.tntmodders.takumi.core.TakumiEntityCore;
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.init.Biomes;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.event.world.ExplosionEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class EntityGlowStoneCreeper extends EntityTakumiAbstractCreeper {
+    public EntityGlowStoneCreeper(World worldIn) {
+        super(worldIn);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public int getBrightnessForRender() {
+        return 15728880;
+    }
+
+    public float getBrightness() {
+        return 1.0F;
+    }
+
+    @Override
+    public boolean takumiExplodeEvent(ExplosionEvent.Detonate event) {
+        for (BlockPos pos : event.getAffectedBlocks()) {
+            event.getWorld().setBlockState(pos, Blocks.GLOWSTONE.getDefaultState());
+        }
+        event.getAffectedBlocks().removeAll(event.getAffectedBlocks());
+        return true;
+    }
+
+    @Override
+    public void takumiExplode() {
+    }
+
+    @Override
+    public EnumTakumiRank takumiRank() {
+        return EnumTakumiRank.LOW;
+    }
+
+    @Override
+    public EnumTakumiType takumiType() {
+        return EnumTakumiType.GROUND;
+    }
+
+    @Override
+    public int getExplosionPower() {
+        return 2;
+    }
+
+    @Override
+    public int getSecondaryColor() {
+        return 12303206;
+    }
+
+    @Override
+    public int getPrimaryColor() {
+        return 16776960;
+    }
+
+    @Override
+    public boolean isCustomSpawn() {
+        return true;
+    }
+
+    @Override
+    public void customSpawn() {
+        EntityRegistry.addSpawn(this.getClass(), this.takumiRank().getSpawnWeight() * 2, 3, 10, EnumCreatureType.MONSTER, Biomes.HELL);
+        EntityRegistry.addSpawn(this.getClass(), this.takumiRank().getSpawnWeight(), 1, 3, EnumCreatureType.MONSTER,
+                TakumiEntityCore.biomes.toArray(new Biome[0]));
+    }
+
+    @Override
+    public String getRegisterName() {
+        return "glowstonecreeper";
+    }
+
+    @Override
+    public int getRegisterID() {
+        return 28;
+    }
+}
