d656601c099a5e829568989c08f4734f09cf3c61
Tom Kate <https://tomkate@bitbucket.org/tntmodders/project_fassion.git>
2017-09-23 19:19:27 +0900

    [41]失匠の追加

---
+++ src/main/java/com/tntmodders/takumi/entity/mobs/EntityLostCreeper.java
@@ -0,0 +1,92 @@
+package com.tntmodders.takumi.entity.mobs;
+
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.ExplosionEvent;
+
+public class EntityLostCreeper extends EntityTakumiAbstractCreeper {
+    public EntityLostCreeper(World worldIn) {
+        super(worldIn);
+    }
+
+    @Override
+    public void takumiExplode() {
+    }
+
+    @Override
+    public boolean takumiExplodeEvent(ExplosionEvent.Detonate event) {
+        for (Entity entity : event.getAffectedEntities()) {
+            if (entity instanceof EntityLivingBase) {
+                ((EntityLivingBase) entity).getHeldItemMainhand().shrink(this.rand.nextInt(5) + 1);
+                ((EntityLivingBase) entity).getHeldItemOffhand().shrink(this.rand.nextInt(5) + 1);
+                if (this.getPowered()) {
+                    EntityEquipmentSlot slot = EntityEquipmentSlot.MAINHAND;
+                    switch (this.rand.nextInt(4)) {
+                        case 0: {
+                            slot = EntityEquipmentSlot.HEAD;
+                            break;
+                        }
+                        case 1: {
+                            slot = EntityEquipmentSlot.CHEST;
+                            break;
+                        }
+                        case 2: {
+                            slot = EntityEquipmentSlot.LEGS;
+                            break;
+                        }
+                        case 3: {
+                            slot = EntityEquipmentSlot.FEET;
+                            break;
+                        }
+                    }
+                    entity.setItemStackToSlot(slot, ItemStack.EMPTY);
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public EnumTakumiRank takumiRank() {
+        return EnumTakumiRank.LOW;
+    }
+
+    @Override
+    public EnumTakumiType takumiType() {
+        return EnumTakumiType.WIND_M;
+    }
+
+    @Override
+    public int getExplosionPower() {
+        return 3;
+    }
+
+    @Override
+    public int getSecondaryColor() {
+        return 4128831;
+    }
+
+    @Override
+    public int getPrimaryColor() {
+        return 0xaa44ff;
+    }
+
+    @Override
+    public boolean isCustomSpawn() {
+        return false;
+    }
+
+    @Override
+    public String getRegisterName() {
+        return "lostcreeper";
+    }
+
+    @Override
+    public int getRegisterID() {
+        return 32;
+    }
+}
