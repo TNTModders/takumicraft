3e092046b5bc440617c10bada849b63f013444c5
Tom Kate <https://tomkate@bitbucket.org/tntmodders/project_fassion.git>
2017-09-30 13:14:42 +0900

    [51][52]牛匠・豚匠の追加

---
+++ src/main/java/com/tntmodders/takumi/entity/mobs/EntityCowCreeper.java
@@ -0,0 +1,139 @@
+package com.tntmodders.takumi.entity.mobs;
+
+import com.tntmodders.takumi.client.render.RenderCowCreeper;
+import com.tntmodders.takumi.core.TakumiEntityCore;
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.storage.loot.LootTableList;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+
+import javax.annotation.Nullable;
+
+public class EntityCowCreeper extends EntityTakumiAbstractCreeper {
+    public EntityCowCreeper(World worldIn) {
+        super(worldIn);
+        this.setSize(0.9F, 1.4F);
+    }
+
+    @Override
+    public void takumiExplode() {
+    }
+
+    @Override
+    public EnumTakumiRank takumiRank() {
+        return EnumTakumiRank.LOW;
+    }
+
+    @Override
+    public EnumTakumiType takumiType() {
+        return EnumTakumiType.NORMAL;
+    }
+
+    @Override
+    public int getExplosionPower() {
+        return 3;
+    }
+
+    @Override
+    public int getSecondaryColor() {
+        return 0xeeffee;
+    }
+
+    @Override
+    public boolean isCustomSpawn() {
+        return true;
+    }
+
+    @Override
+    public String getRegisterName() {
+        return "cowcreeper";
+    }
+
+    @Override
+    public int getRegisterID() {
+        return 38;
+    }
+
+    @Override
+    protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(10.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.20000000298023224D);
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource p_184601_1_) {
+        return SoundEvents.ENTITY_COW_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ENTITY_COW_DEATH;
+    }
+
+    @Override
+    @Nullable
+    protected ResourceLocation getLootTable() {
+        return LootTableList.ENTITIES_COW;
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.ENTITY_COW_AMBIENT;
+    }
+
+    @Override
+    protected void playStepSound(BlockPos pos, Block blockIn) {
+        this.playSound(SoundEvents.ENTITY_COW_STEP, 0.15F, 1.0F);
+    }
+
+    @Override
+    public EntityItem entityDropItem(ItemStack stack, float offsetY) {
+        if (stack.getItem() == Items.BEEF) {
+            stack = new ItemStack(Items.COOKED_BEEF, stack.getCount());
+        }
+        return super.entityDropItem(stack, offsetY);
+    }
+
+    @Override
+    public float getEyeHeight() {
+        return this.isChild() ? this.height : 1.3F;
+    }
+
+    /**
+     * Returns the volume for the sounds this mob makes.
+     */
+    @Override
+    protected float getSoundVolume() {
+        return 0.4F;
+    }
+
+    @Override
+    public void customSpawn() {
+        EntityRegistry.addSpawn(this.getClass(), this.takumiRank().getSpawnWeight(), 1, 3, TakumiEntityCore.CREATURE_TAKUMI
+                , TakumiEntityCore.biomes.toArray(new Biome[0]));
+    }
+
+    @Override
+    public int getPrimaryColor() {
+        return 0x001100;
+    }
+
+    @Override
+    public RenderLiving getRender(RenderManager manager) {
+        return new RenderCowCreeper(manager);
+    }
+}
---
+++ src/main/java/com/tntmodders/takumi/entity/mobs/EntityPigCreeper.java
@@ -0,0 +1,134 @@
+package com.tntmodders.takumi.entity.mobs;
+
+import com.tntmodders.takumi.client.render.RenderPigCreeper;
+import com.tntmodders.takumi.core.TakumiEntityCore;
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.storage.loot.LootTableList;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+
+import javax.annotation.Nullable;
+
+public class EntityPigCreeper extends EntityTakumiAbstractCreeper {
+    public EntityPigCreeper(World worldIn) {
+        super(worldIn);
+        this.setSize(0.9F, 0.9F);
+    }
+
+    @Override
+    public void takumiExplode() {
+    }
+
+    @Override
+    public EnumTakumiRank takumiRank() {
+        return EnumTakumiRank.LOW;
+    }
+
+    @Override
+    public EnumTakumiType takumiType() {
+        return EnumTakumiType.NORMAL;
+    }
+
+    @Override
+    public int getExplosionPower() {
+        return 3;
+    }
+
+    @Override
+    public int getSecondaryColor() {
+        return 0xeeffee;
+    }
+
+    @Override
+    public boolean isCustomSpawn() {
+        return true;
+    }
+
+    @Override
+    public String getRegisterName() {
+        return "pigcreeper";
+    }
+
+    @Override
+    public int getRegisterID() {
+        return 39;
+    }
+
+    @Override
+    protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(10.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource p_184601_1_) {
+        return SoundEvents.ENTITY_PIG_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ENTITY_PIG_DEATH;
+    }
+
+    @Override
+    @Nullable
+    protected ResourceLocation getLootTable() {
+        return LootTableList.ENTITIES_PIG;
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.ENTITY_PIG_AMBIENT;
+    }
+
+    @Override
+    protected void playStepSound(BlockPos pos, Block blockIn) {
+        this.playSound(SoundEvents.ENTITY_PIG_STEP, 0.15F, 1.0F);
+    }
+
+    @Override
+    public EntityItem entityDropItem(ItemStack stack, float offsetY) {
+        if (stack.getItem() == Items.PORKCHOP) {
+            stack = new ItemStack(Items.COOKED_PORKCHOP, stack.getCount());
+        }
+        return super.entityDropItem(stack, offsetY);
+    }
+
+    /**
+     * Returns the volume for the sounds this mob makes.
+     */
+    @Override
+    protected float getSoundVolume() {
+        return 0.4F;
+    }
+
+    @Override
+    public void customSpawn() {
+        EntityRegistry.addSpawn(this.getClass(), this.takumiRank().getSpawnWeight(), 1, 3, TakumiEntityCore.CREATURE_TAKUMI
+                , TakumiEntityCore.biomes.toArray(new Biome[0]));
+    }
+
+    @Override
+    public int getPrimaryColor() {
+        return 0xeeeeff;
+    }
+
+    @Override
+    public RenderLiving getRender(RenderManager manager) {
+        return new RenderPigCreeper(manager);
+    }
+}
