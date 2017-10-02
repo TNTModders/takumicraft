0f99cb97f2243d4fd3516894bbd9d4ade4bce6e5
Tom Kate <https://tomkate@bitbucket.org/tntmodders/project_fassion.git>
2017-10-02 23:41:23 +0900

    [55]音匠の追加

---
+++ src/main/java/com/tntmodders/takumi/entity/mobs/EntityMusicCreeper.java
@@ -0,0 +1,85 @@
+package com.tntmodders.takumi.entity.mobs;
+
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.monster.EntityCreeper;
+import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+
+import java.lang.reflect.Field;
+
+public class EntityMusicCreeper extends EntityTakumiAbstractCreeper {
+    public EntityMusicCreeper(World worldIn) {
+        super(worldIn);
+    }
+
+    @Override
+    public void takumiExplode() {
+    }
+
+    @Override
+    public EnumTakumiRank takumiRank() {
+        return EnumTakumiRank.LOW;
+    }
+
+    @Override
+    public EnumTakumiType takumiType() {
+        return EnumTakumiType.WIND_M;
+    }
+
+    @Override
+    public int getExplosionPower() {
+        return 3;
+    }
+
+    @Override
+    public int getSecondaryColor() {
+        return 0xffaa00;
+    }
+
+    @Override
+    public boolean isCustomSpawn() {
+        return false;
+    }
+
+    @Override
+    public String getRegisterName() {
+        return "musiccreeper";
+    }
+
+    @Override
+    public int getRegisterID() {
+        return 42;
+    }
+
+    @Override
+    public void onUpdate() {
+        if (this.isEntityAlive() && this.rand.nextInt(10) == 0) {
+            this.world.spawnParticle(EnumParticleTypes.NOTE, this.posX + rand.nextFloat() - 0.5, this.posY + rand.nextFloat(), this.posZ + rand.nextFloat() - 0.5,
+                    -this.motionX, -this.motionY, -this.motionZ);
+        }
+        try {
+            Field field = EntityCreeper.class.getDeclaredField("timeSinceIgnited");
+            field.setAccessible(true);
+            int timeSinceIgnited = ((int) field.get(this));
+            if (timeSinceIgnited == 30 && FMLCommonHandler.instance().getSide().isClient()) {
+                Minecraft.getMinecraft().world.playRecord(this.getPosition(), SoundEvents.RECORD_13);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        super.onUpdate();
+    }
+
+    @Override
+    public void onDeath(DamageSource cause) {
+        if (!this.world.isRemote) {
+            this.dropItem(Items.RECORD_13, 1);
+        }
+        super.onDeath(cause);
+    }
+}
