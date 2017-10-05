01890784b22cfd5f7eac790fd0adf531ad74ded0
Tom Kate <https://tomkate@bitbucket.org/tntmodders/project_fassion.git>
2017-10-05 20:04:08 +0900

    [57]狼匠の追加

---
+++ src/main/java/com/tntmodders/takumi/entity/mobs/EntityWolfCreeper.java
@@ -0,0 +1,289 @@
+package com.tntmodders.takumi.entity.mobs;
+
+import com.tntmodders.takumi.client.render.RenderWolfCreeper;
+import com.tntmodders.takumi.core.TakumiEntityCore;
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.passive.EntityWolf;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.storage.loot.LootTableList;
+import net.minecraftforge.event.world.ExplosionEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EntityWolfCreeper extends EntityTakumiAbstractCreeper {
+    private float headRotationCourse;
+    private float headRotationCourseOld;
+    /**
+     * true is the wolf is wet else false
+     */
+    private boolean isWet;
+    /**
+     * True if the wolf is shaking else False
+     */
+    private boolean isShaking;
+    /**
+     * This time increases while wolf is shaking and emitting water particles.
+     */
+    private float timeWolfIsShaking;
+    private float prevTimeWolfIsShaking;
+
+    public EntityWolfCreeper(World worldIn) {
+        super(worldIn);
+        this.setSize(0.6F, 0.85F);
+    }
+
+    /**
+     * True if the wolf is wet
+     */
+    @SideOnly(Side.CLIENT)
+    public boolean isWolfWet() {
+        return this.isWet;
+    }
+
+    /**
+     * Used when calculating the amount of shading to apply while the wolf is wet.
+     */
+    @SideOnly(Side.CLIENT)
+    public float getShadingWhileWet(float p_70915_1_) {
+        return 0.75F + (this.prevTimeWolfIsShaking + (this.timeWolfIsShaking - this.prevTimeWolfIsShaking) * p_70915_1_) / 2.0F * 0.25F;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public float getShakeAngle(float p_70923_1_, float p_70923_2_) {
+        float f = (this.prevTimeWolfIsShaking + (this.timeWolfIsShaking - this.prevTimeWolfIsShaking) * p_70923_1_ + p_70923_2_) / 1.8F;
+
+        if (f < 0.0F) {
+            f = 0.0F;
+        } else if (f > 1.0F) {
+            f = 1.0F;
+        }
+
+        return MathHelper.sin(f * (float) Math.PI) * MathHelper.sin(f * (float) Math.PI * 11.0F) * 0.15F * (float) Math.PI;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public float getInterestedAngle(float p_70917_1_) {
+        return (this.headRotationCourseOld + (this.headRotationCourse - this.headRotationCourseOld) * p_70917_1_) * 0.15F * (float) Math.PI;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void handleStatusUpdate(byte id) {
+        if (id == 8) {
+            this.isShaking = true;
+            this.timeWolfIsShaking = 0.0F;
+            this.prevTimeWolfIsShaking = 0.0F;
+        } else {
+            super.handleStatusUpdate(id);
+        }
+    }
+
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.ENTITY_WOLF_GROWL;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public float getTailRotation() {
+        return 1.5393804F;
+
+    }
+
+    @Override
+    public void takumiExplode() {
+        if (!this.world.isRemote) {
+            int r = this.getPowered() ? 5 : 2;
+            for (int t = 0; t < r * 1.5; t++) {
+                EntityWolf wolf = new EntityWolf(this.world);
+                wolf.setPosition(this.posX, this.posY, this.posZ);
+                wolf.setAttackTarget(this.getAttackTarget());
+                wolf.setAngry(true);
+                wolf.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.65);
+                this.world.spawnEntity(wolf);
+            }
+        }
+    }
+
+    @Override
+    public EnumTakumiRank takumiRank() {
+        return EnumTakumiRank.LOW;
+    }
+
+    @Override
+    public EnumTakumiType takumiType() {
+        return EnumTakumiType.NORMAL;
+    }
+
+    @Override
+    public int getExplosionPower() {
+        return 3;
+    }
+
+    @Override
+    public int getSecondaryColor() {
+        return 0xeeffee;
+    }
+
+    @Override
+    public boolean isCustomSpawn() {
+        return true;
+    }
+
+    @Override
+    public String getRegisterName() {
+        return "wolfcreeper";
+    }
+
+    @Override
+    public int getRegisterID() {
+        return 44;
+    }
+
+    @Override
+    public boolean takumiExplodeEvent(ExplosionEvent.Detonate event) {
+        List<EntityWolf> list = new ArrayList<>();
+        for (Entity entity : event.getAffectedEntities()) {
+            if (entity instanceof EntityWolf) {
+                list.add(((EntityWolf) entity));
+            }
+        }
+        event.getAffectedEntities().removeAll(list);
+        return true;
+    }
+
+    @Override
+    public void customSpawn() {
+        EntityRegistry.addSpawn(this.getClass(), this.takumiRank().getSpawnWeight(), 1, 3, TakumiEntityCore.CREATURE_TAKUMI
+                , TakumiEntityCore.biomes.toArray(new Biome[0]));
+    }
+
+    @Override
+    public int getPrimaryColor() {
+        return 0x664400;
+    }
+
+    @Override
+    public RenderLiving getRender(RenderManager manager) {
+        return new RenderWolfCreeper<>(manager);
+    }
+
+    @Override
+    protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.30000001192092896D);
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(8.0D);
+    }
+
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+        this.headRotationCourseOld = this.headRotationCourse;
+        this.headRotationCourse += (0.0F - this.headRotationCourse) * 0.4F;
+        if (this.isWet()) {
+            this.isWet = true;
+            this.isShaking = false;
+            this.timeWolfIsShaking = 0.0F;
+            this.prevTimeWolfIsShaking = 0.0F;
+        } else if ((this.isWet || this.isShaking) && this.isShaking) {
+            if (this.timeWolfIsShaking == 0.0F) {
+                this.playSound(SoundEvents.ENTITY_WOLF_SHAKE, this.getSoundVolume(), (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F + 1.0F);
+            }
+
+            this.prevTimeWolfIsShaking = this.timeWolfIsShaking;
+            this.timeWolfIsShaking += 0.05F;
+
+            if (this.prevTimeWolfIsShaking >= 2.0F) {
+                this.isWet = false;
+                this.isShaking = false;
+                this.prevTimeWolfIsShaking = 0.0F;
+                this.timeWolfIsShaking = 0.0F;
+            }
+
+            if (this.timeWolfIsShaking > 0.4F) {
+                float f = (float) this.getEntityBoundingBox().minY;
+                int i = (int) (MathHelper.sin((this.timeWolfIsShaking - 0.4F) * (float) Math.PI) * 7.0F);
+
+                for (int j = 0; j < i; ++j) {
+                    float f1 = (this.rand.nextFloat() * 2.0F - 1.0F) * this.width * 0.5F;
+                    float f2 = (this.rand.nextFloat() * 2.0F - 1.0F) * this.width * 0.5F;
+                    this.world.spawnParticle(EnumParticleTypes.WATER_SPLASH, this.posX + (double) f1, (double) (f + 0.8F), this.posZ + (double) f2, this.motionX, this.motionY, this.motionZ);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected SoundEvent getHurtSound(DamageSource p_184601_1_) {
+        return SoundEvents.ENTITY_WOLF_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ENTITY_WOLF_DEATH;
+    }
+
+    @Override
+    public boolean attackEntityAsMob(Entity entityIn) {
+        boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), (float) ((int) this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue()));
+
+        if (flag) {
+            this.applyEnchantments(this, entityIn);
+        }
+
+        return flag;
+    }
+
+    @Override
+    @Nullable
+    protected ResourceLocation getLootTable() {
+        return LootTableList.ENTITIES_WOLF;
+    }
+
+    /**
+     * Returns the volume for the sounds this mob makes.
+     */
+    @Override
+    protected float getSoundVolume() {
+        return 0.4F;
+    }
+
+    @Override
+    public void onLivingUpdate() {
+        super.onLivingUpdate();
+
+        if (!this.world.isRemote && this.isWet && !this.isShaking && !this.hasPath() && this.onGround) {
+            this.isShaking = true;
+            this.timeWolfIsShaking = 0.0F;
+            this.prevTimeWolfIsShaking = 0.0F;
+            this.world.setEntityState(this, (byte) 8);
+        }
+    }
+
+    @Override
+    protected void playStepSound(BlockPos pos, Block blockIn) {
+        this.playSound(SoundEvents.ENTITY_WOLF_STEP, 0.15F, 1.0F);
+    }
+
+    @Override
+    public float getEyeHeight() {
+        return this.height * 0.8F;
+    }
+}
