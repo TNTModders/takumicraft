af08100c1ae10134189c77d498bac396d35cdbaa
Tom Kate <https://tomkate@bitbucket.org/tntmodders/project_fassion.git>
2017-12-23 12:20:11 +0900

    [102]白熊匠の追加

---
+++ src/main/java/com/tntmodders/takumi/entity/mobs/EntityPolarBearCreeper.java
@@ -0,0 +1,272 @@
+package com.tntmodders.takumi.entity.mobs;
+
+import com.tntmodders.takumi.client.render.RenderPolarBearCreeper;
+import com.tntmodders.takumi.core.TakumiEntityCore;
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.*;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.Biome.TempCategory;
+import net.minecraft.world.storage.loot.LootTableList;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class EntityPolarBearCreeper extends EntityTakumiAbstractCreeper {
+    
+    private static final DataParameter <Boolean> IS_STANDING = EntityDataManager.createKey(EntityPolarBearCreeper.class, DataSerializers.BOOLEAN);
+    private float clientSideStandAnimation0;
+    private float clientSideStandAnimation;
+    private int warningSoundTicks;
+    
+    public EntityPolarBearCreeper(World worldIn) {
+        super(worldIn);
+        this.setSize(1.3F, 1.4F);
+    }
+    
+    @Override
+    protected void initEntityAI() {
+        super.initEntityAI();
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new AIMeleeAttack());
+        this.tasks.addTask(1, new AIPanic());
+        this.tasks.addTask(5, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(7, new EntityAILookIdle(this));
+        this.targetTasks.addTask(1, new EntityAINearestAttackableTarget <>(this, EntityPlayer.class, true));
+    }
+    
+    @Override
+    protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(30.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(20.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
+        this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(6.0D);
+    }
+    
+    @Override
+    protected void entityInit() {
+        super.entityInit();
+        this.dataManager.register(IS_STANDING, Boolean.FALSE);
+    }
+    
+    @Override
+    public void onUpdate() {
+        super.onUpdate();
+        
+        if (this.world.isRemote) {
+            this.clientSideStandAnimation0 = this.clientSideStandAnimation;
+            
+            if (this.isStanding()) {
+                this.clientSideStandAnimation = MathHelper.clamp(this.clientSideStandAnimation + 1.0F, 0.0F, 6.0F);
+            } else {
+                this.clientSideStandAnimation = MathHelper.clamp(this.clientSideStandAnimation - 1.0F, 0.0F, 6.0F);
+            }
+        }
+        
+        if (this.warningSoundTicks > 0) {
+            --this.warningSoundTicks;
+        }
+    }
+    
+    @Override
+    protected SoundEvent getHurtSound(DamageSource p_184601_1_) {
+        return SoundEvents.ENTITY_POLAR_BEAR_HURT;
+    }
+    
+    @Override
+    protected SoundEvent getDeathSound() {
+        return SoundEvents.ENTITY_POLAR_BEAR_DEATH;
+    }
+    
+    @Override
+    public boolean attackEntityAsMob(Entity entityIn) {
+        boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), (float) (int) this.getEntityAttribute(SharedMonsterAttributes
+                .ATTACK_DAMAGE).getAttributeValue());
+        if (!this.world.isRemote) {
+            this.world.createExplosion(this, this.posX, this.posY, this.posZ, 3, true);
+        }
+        if (flag) {
+            this.applyEnchantments(this, entityIn);
+        }
+        
+        return flag;
+    }
+    
+    @Override
+    @Nullable
+    protected ResourceLocation getLootTable() {
+        return LootTableList.ENTITIES_POLAR_BEAR;
+    }
+    
+    public boolean isStanding() {
+        return this.dataManager.get(IS_STANDING);
+    }
+    
+    public void setStanding(boolean standing) {
+        this.dataManager.set(IS_STANDING, standing);
+    }
+    
+    @Override
+    protected SoundEvent getAmbientSound() {
+        return this.isChild() ? SoundEvents.ENTITY_POLAR_BEAR_BABY_AMBIENT : SoundEvents.ENTITY_POLAR_BEAR_AMBIENT;
+    }
+    
+    @Override
+    protected void playStepSound(BlockPos pos, Block blockIn) {
+        this.playSound(SoundEvents.ENTITY_POLAR_BEAR_STEP, 0.15F, 1.0F);
+    }
+    
+    protected void playWarningSound() {
+        if (this.warningSoundTicks <= 0) {
+            this.playSound(SoundEvents.ENTITY_POLAR_BEAR_WARNING, 1.0F, 1.0F);
+            this.warningSoundTicks = 40;
+        }
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public float getStandingAnimationScale(float p_189795_1_) {
+        return (this.clientSideStandAnimation0 + (this.clientSideStandAnimation - this.clientSideStandAnimation0) * p_189795_1_) / 6.0F;
+    }
+    
+    @Override
+    protected float getWaterSlowDown() {
+        return 0.98F;
+    }
+    
+    @Override
+    public void takumiExplode() {
+    }
+    
+    @Override
+    public EnumTakumiRank takumiRank() {
+        return EnumTakumiRank.MID;
+    }
+    
+    @Override
+    public EnumTakumiType takumiType() {
+        return EnumTakumiType.NORMAL;
+    }
+    
+    @Override
+    public int getExplosionPower() {
+        return 3;
+    }
+    
+    @Override
+    public int getSecondaryColor() {
+        return 0x005588;
+    }
+    
+    @Override
+    public boolean isCustomSpawn() {
+        return true;
+    }
+    
+    @Override
+    public String getRegisterName() {
+        return "polarbearcreeper";
+    }
+    
+    @Override
+    public int getRegisterID() {
+        return 246;
+    }
+    
+    @Override
+    public void customSpawn() {
+        List <Biome> biomeList = TakumiEntityCore.biomes;
+        biomeList.removeIf(biome -> biome.getTempCategory() != TempCategory.COLD);
+        EntityRegistry.addSpawn(this.getClass(), this.takumiRank().getSpawnWeight() * 25, 5, 20, TakumiEntityCore.CREATURE_TAKUMI, biomeList
+                .toArray(new Biome[0]));
+    }
+    
+    @Override
+    public int getPrimaryColor() {
+        return 0x55ff55;
+    }
+    
+    @Override
+    public Object getRender(RenderManager manager) {
+        return new RenderPolarBearCreeper <>(manager);
+    }
+    
+    class AIMeleeAttack extends EntityAIAttackMelee {
+        
+        public AIMeleeAttack() {
+            super(EntityPolarBearCreeper.this, 1.25D, true);
+        }
+        
+        /**
+         * Reset the task's internal state. Called when this task is interrupted by another one
+         */
+        @Override
+        public void resetTask() {
+            EntityPolarBearCreeper.this.setStanding(false);
+            super.resetTask();
+        }
+        
+        @Override
+        protected void checkAndPerformAttack(EntityLivingBase p_190102_1_, double p_190102_2_) {
+            double d0 = this.getAttackReachSqr(p_190102_1_);
+            
+            if (p_190102_2_ <= d0 && this.attackTick <= 0) {
+                this.attackTick = 20;
+                this.attacker.attackEntityAsMob(p_190102_1_);
+                EntityPolarBearCreeper.this.setStanding(false);
+            } else if (p_190102_2_ <= d0 * 2.0D) {
+                if (this.attackTick <= 0) {
+                    EntityPolarBearCreeper.this.setStanding(false);
+                    this.attackTick = 20;
+                }
+                
+                if (this.attackTick <= 10) {
+                    EntityPolarBearCreeper.this.setStanding(true);
+                    EntityPolarBearCreeper.this.playWarningSound();
+                }
+            } else {
+                this.attackTick = 20;
+                EntityPolarBearCreeper.this.setStanding(false);
+            }
+        }
+        
+        @Override
+        protected double getAttackReachSqr(EntityLivingBase attackTarget) {
+            return (double) (4.0F + attackTarget.width);
+        }
+    }
+    
+    class AIPanic extends EntityAIPanic {
+        
+        public AIPanic() {
+            super(EntityPolarBearCreeper.this, 2.0D);
+        }
+        
+        /**
+         * Returns whether the EntityAIBase should begin execution.
+         */
+        @Override
+        public boolean shouldExecute() {
+            return (EntityPolarBearCreeper.this.isChild() || EntityPolarBearCreeper.this.isBurning()) && super.shouldExecute();
+        }
+    }
+}
