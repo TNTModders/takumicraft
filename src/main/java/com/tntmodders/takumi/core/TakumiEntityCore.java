4ab0e04c31611bf22f6a17b12ff05409d5654b97
Tom Kate <https://tomkate@bitbucket.org/tntmodders/project_fassion.git>
2018-01-21 20:59:39 +0900

    [112]忍匠の追加

--- src/main/java/com/tntmodders/takumi/core/TakumiEntityCore.java
+++ src/main/java/com/tntmodders/takumi/core/TakumiEntityCore.java
@@ -66,7 +66,7 @@ public class TakumiEntityCore {
                         .class, EntityYukariCreeper.class, EntityZombieCreeper.class, EntityZombieVillagerCreeper.class, EntityCraftsmanCreeper
                         .class, EntityLlamaCreeper.class, EntityPolarBearCreeper.class, EntityShulkerCreeper.class, EntityConcreteCreeper.class,
                 EntityVindicatorCreeper.class, EntityEvokerCreeper.class, EntityVexCreeper.class, EntityIllusionerCreeper.class,
-                EntityPigmanCreeper.class, EntityKillerCreeper.class, EntityExperienceCreeper.class));
+                EntityPigmanCreeper.class, EntityKillerCreeper.class, EntityExperienceCreeper.class,EntitySinobiCreeper.class));
     }
     
     public static List <ITakumiEntity> getEntityList() {
