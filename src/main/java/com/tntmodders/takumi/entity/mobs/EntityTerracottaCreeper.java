0bd38727d34a7a3481bfaa56c90379fac044b021
Tom Kate <https://tomkate@bitbucket.org/tntmodders/project_fassion.git>
2018-02-23 22:22:44 +0900

    [120]テラコッタ匠の追加

---
+++ src/main/java/com/tntmodders/takumi/entity/mobs/EntityTerracottaCreeper.java
@@ -0,0 +1,64 @@
+package com.tntmodders.takumi.entity.mobs;
+
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.ExplosionEvent.Detonate;
+
+public class EntityTerracottaCreeper extends EntityTakumiAbstractCreeper {
+    
+    public EntityTerracottaCreeper(World worldIn) {
+        super(worldIn);
+    }
+    
+    @Override
+    public boolean takumiExplodeEvent(Detonate event) {
+        event.getAffectedBlocks().forEach(pos -> world.setBlockState(pos, Blocks.CYAN_GLAZED_TERRACOTTA.getDefaultState()));
+        event.getAffectedBlocks().removeAll(event.getAffectedBlocks());
+        return true;
+    }
+    
+    @Override
+    public int getPrimaryColor() {
+        return 0x000066;
+    }
+    
+    @Override
+    public void takumiExplode() {
+    }
+    
+    @Override
+    public EnumTakumiRank takumiRank() {
+        return EnumTakumiRank.LOW;
+    }
+    
+    @Override
+    public EnumTakumiType takumiType() {
+        return EnumTakumiType.GROUND_M;
+    }
+    
+    @Override
+    public int getExplosionPower() {
+        return 3;
+    }
+    
+    @Override
+    public int getSecondaryColor() {
+        return 12303206;
+    }
+    
+    @Override
+    public boolean isCustomSpawn() {
+        return false;
+    }
+    
+    @Override
+    public String getRegisterName() {
+        return "terracottacreeper";
+    }
+    
+    @Override
+    public int getRegisterID() {
+        return 52;
+    }
+}
