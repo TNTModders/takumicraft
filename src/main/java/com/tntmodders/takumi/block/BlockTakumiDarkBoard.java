5fd59ec8086de003a89a9ee37ca04c2c9fffd7dc
TomKate <tomo23katakata@gmail.com>
2018-03-12 22:26:23 +0900

    石版に登録アイテムを追加

--- src/main/java/com/tntmodders/takumi/block/BlockTakumiDarkBoard.java
+++ src/main/java/com/tntmodders/takumi/block/BlockTakumiDarkBoard.java
@@ -1,13 +1,17 @@
 package com.tntmodders.takumi.block;
 
 import com.tntmodders.takumi.TakumiCraftCore;
+import com.tntmodders.takumi.core.TakumiBlockCore;
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
 import com.tntmodders.takumi.tileentity.TileEntityDarkBoard;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -15,6 +19,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
+import java.util.Random;
 
 public class BlockTakumiDarkBoard extends BlockContainer {
     public BlockTakumiDarkBoard() {
@@ -31,6 +36,37 @@ public class BlockTakumiDarkBoard extends BlockContainer {
     @Override
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new TileEntityDarkBoard();
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
+            EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        if (worldIn.getTileEntity(pos) instanceof TileEntityDarkBoard) {
+            TakumiCraftCore.LOGGER.info(((TileEntityDarkBoard) worldIn.getTileEntity(pos)).name);
+        }
+        return super.onBlockActivated(worldIn, pos, state, playerIn, hand, facing, hitX, hitY, hitZ);
+    }
+
+    @Override
+    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+        super.onBlockAdded(worldIn, pos, state);
+        if (worldIn.getTileEntity(pos) instanceof TileEntityDarkBoard) {
+            Random random = new Random();
+            random.setSeed(System.currentTimeMillis() + pos.getX() + pos.getZ());
+            if (((TileEntityDarkBoard) worldIn.getTileEntity(pos)).name == null ||
+                    ((TileEntityDarkBoard) worldIn.getTileEntity(pos)).name.isEmpty()) {
+                Class clazz = ((Class) TakumiBlockCore.BOMB_MAP.keySet().toArray()[random
+                        .nextInt(TakumiBlockCore.BOMB_MAP.size())]);
+                try {
+                    EntityTakumiAbstractCreeper creeper =
+                            (EntityTakumiAbstractCreeper) clazz.getConstructor(World.class).newInstance(worldIn);
+                    ((TileEntityDarkBoard) worldIn.getTileEntity(pos)).name = creeper.getRegisterName();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            TakumiCraftCore.LOGGER.info(((TileEntityDarkBoard) worldIn.getTileEntity(pos)).name);
+        }
     }
 
     @Override
--- src/main/java/com/tntmodders/takumi/tileentity/TileEntityDarkBoard.java
+++ src/main/java/com/tntmodders/takumi/tileentity/TileEntityDarkBoard.java
@@ -1,6 +1,48 @@
 package com.tntmodders.takumi.tileentity;
 
+import com.tntmodders.takumi.TakumiCraftCore;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityDarkBoard extends TileEntity {
+    public String name;
+
+    public TileEntityDarkBoard() {
+        super();
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+        if (this.name == null || this.name.isEmpty()) {
+            this.name = compound.getString("monster");
+        }
+    }
+
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket() {
+        return new SPacketUpdateTileEntity(this.pos, 0, this.getUpdateTag());
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity pkt) {
+        super.onDataPacket(net, pkt);
+        this.readFromNBT(pkt.getNbtCompound());
+        TakumiCraftCore.LOGGER.info("pkt");
+    }
+
+    @Override
+    public NBTTagCompound getUpdateTag() {
+        return this.writeToNBT(new NBTTagCompound());
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+        if (this.name != null) {
+            compound.setString("monster", this.name);
+        }
+        return super.writeToNBT(compound);
+    }
 }
