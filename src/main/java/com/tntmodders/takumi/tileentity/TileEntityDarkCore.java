fb80d1581cf6d2f2f70bc7173213a430554b86d4
TomKate <tomo23katakata@gmail.com>
2018-03-18 17:50:19 +0900

    召喚時振動エフェクトの改良

--- src/main/java/com/tntmodders/takumi/tileentity/TileEntityDarkCore.java
+++ src/main/java/com/tntmodders/takumi/tileentity/TileEntityDarkCore.java
@@ -2,9 +2,13 @@ package com.tntmodders.takumi.tileentity;
 
 import com.tntmodders.takumi.core.TakumiBlockCore;
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ITickable;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class TileEntityDarkCore extends TileEntity implements ITickable {
@@ -22,12 +26,49 @@ public class TileEntityDarkCore extends TileEntity implements ITickable {
             if (FMLCommonHandler.instance().getSide().isClient()) {
                 for (int x = -10; x <= 10; x++) {
                     for (int z = -10; z <= 10; z++) {
-                        this.world.spawnParticle(EnumParticleTypes.BLOCK_CRACK, this.getPos().getX() + x,
-                                this.getPos().getY() + 1, this.getPos().getZ() + z, 0, 0, 0,
-                                Block.getIdFromBlock(this.world.getBlockState(this.getPos().add(x, 0, z)).getBlock()));
+                        for (int y = 0; y <= 4; y++) {
+                            Block block = this.world.getBlockState(this.getPos().add(x, y, z)).getBlock();
+                            if (block == TakumiBlockCore.DARKBRICK) {
+                                if (this.world.rand.nextBoolean()) {
+                                    if (this.world.rand.nextBoolean()) {
+                                        this.world.spawnParticle(EnumParticleTypes.BLOCK_CRACK,
+                                                this.getPos().getX() + x, this.getPos().getY() + y + 1,
+                                                this.getPos().getZ() + z, 0, 0, 0, Block.getIdFromBlock(block));
+                                    } else {
+                                        this.world.spawnParticle(EnumParticleTypes.BLOCK_CRACK,
+                                                this.getPos().getX() + x, this.getPos().getY() + y - 1,
+                                                this.getPos().getZ() + z, 0, 0, 0, Block.getIdFromBlock(block));
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
+            this.world.playerEntities.forEach(entityPlayer -> {
+                if (entityPlayer.posY >= this.getPos().getY() && entityPlayer.posY <= this.getPos().getY() + 1 &&
+                        entityPlayer.posX >= this.getPos().getX() - 10 &&
+                        entityPlayer.posX <= this.getPos().getX() + 10 &&
+                        entityPlayer.posZ >= this.getPos().getZ() - 10 &&
+                        entityPlayer.posZ <= this.getPos().getZ() + 10 && entityPlayer.onGround) {
+                    this.jump(entityPlayer);
+                }
+            });
+            this.world.playSound(this.pos.getX() + 0.5, this.getPos().getY() + 0.5, this.getPos().getZ() + 0.5,
+                    SoundEvents.BLOCK_STONE_BREAK, SoundCategory.BLOCKS, 5, -8, false);
         }
+    }
+
+    private void jump(EntityPlayer player) {
+        player.motionY = 0.105;
+
+        if (player.isSprinting()) {
+            float f = player.rotationYaw * 0.017453292F;
+            player.motionX -= (double) (MathHelper.sin(f) * 0.2F);
+            player.motionZ += (double) (MathHelper.cos(f) * 0.2F);
+        }
+
+        player.isAirBorne = true;
+        net.minecraftforge.common.ForgeHooks.onLivingJump(player);
     }
 }
