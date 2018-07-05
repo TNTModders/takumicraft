bb4c2f9b79696c2204976d60fa814bf3a20311af
TomKate <tomo23katakata@gmail.com>
2018-07-05 22:59:24 +0900

    匠式弩砲に高性能爆弾を装填できるように変更

--- src/main/java/com/tntmodders/takumi/entity/item/EntityTakumiArrow.java
+++ src/main/java/com/tntmodders/takumi/entity/item/EntityTakumiArrow.java
@@ -1,8 +1,10 @@
 package com.tntmodders.takumi.entity.item;
 
+import com.tntmodders.takumi.entity.mobs.EntityBoltCreeper;
 import com.tntmodders.takumi.item.ItemTakumiArrow;
 import com.tntmodders.takumi.utils.TakumiUtils;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -17,9 +19,12 @@ public class EntityTakumiArrow extends EntityArrow {
     private int pierce;
     private int power;
     private boolean destroy;
+    private Class<? extends EntityCreeper> container;
+    private EnumArrowType type;
 
     public EntityTakumiArrow(World worldIn) {
         super(worldIn);
+        this.type = EnumArrowType.NORMAL;
     }
 
     public EntityTakumiArrow(World worldIn, ItemStack itemStack, EntityLivingBase base) {
@@ -28,7 +33,33 @@ public class EntityTakumiArrow extends EntityArrow {
         this.pierce = ((ItemTakumiArrow) stack.getItem()).pierce;
         this.power = ((ItemTakumiArrow) stack.getItem()).power;
         this.destroy = ((ItemTakumiArrow) stack.getItem()).destroy;
+        this.container = null;
+        this.type = EnumArrowType.NORMAL;
     }
+
+    public EntityTakumiArrow(World worldIn, ItemStack itemStack, EntityLivingBase base,
+            Class<? extends EntityCreeper> creeper) {
+        this(worldIn, itemStack, base);
+        this.type = EnumArrowType.MONSTER;
+        this.container = creeper;
+    }
+
+    public EntityTakumiArrow(World worldIn, ItemStack itemStack, EntityLivingBase base, EnumArrowType type) {
+        this(worldIn, itemStack, base);
+        this.type = type;
+    }
+
+    public EntityTakumiArrow(World worldIn, EntityLivingBase base, int pierce, int power, boolean destroy,
+            Class<EntityCreeper> container, EnumArrowType type) {
+        super(worldIn, base);
+        this.stack = null;
+        this.pierce = pierce;
+        this.power = power;
+        this.destroy = destroy;
+        this.container = null;
+        this.type = type;
+    }
+
 
     @Override
     protected void onHit(RayTraceResult raytraceResultIn) {
@@ -36,11 +67,40 @@ public class EntityTakumiArrow extends EntityArrow {
             if (raytraceResultIn.entityHit == this.shootingEntity) {
                 return;
             }
-            raytraceResultIn.entityHit
-                    .attackEntityFrom(new EntityDamageSourceIndirect("explosion.player", this.shootingEntity, this),
-                            15.0f);
+            raytraceResultIn.entityHit.attackEntityFrom(
+                    new EntityDamageSourceIndirect("explosion.player", this.shootingEntity, this), 15.0f);
         }
-        TakumiUtils.takumiCreateExplosion(world, this, this.posX, this.posY, this.posZ, power, false, false);
+        if (this.type == null) {
+            this.type = EnumArrowType.NORMAL;
+        }
+        switch (this.type) {
+            case NORMAL: {
+                TakumiUtils.takumiCreateExplosion(world, this, this.posX, this.posY, this.posZ, power, false, destroy);
+                break;
+            }
+            case MONSTER: {
+                try {
+                    EntityCreeper creeper = this.container.getConstructor(World.class).newInstance(world);
+                    creeper.setPosition(this.posX, this.posY, this.posZ);
+                    NBTTagCompound compound = new NBTTagCompound();
+                    creeper.writeEntityToNBT(compound);
+                    compound.setShort("Fuse", (short) 1);
+                    creeper.readEntityFromNBT(compound);
+                    if (creeper instanceof EntityBoltCreeper || world.isThundering()) {
+                        creeper.onStruckByLightning(null);
+                    }
+                    creeper.setInvisible(true);
+                    creeper.ignite();
+                    world.spawnEntity(creeper);
+                    creeper.onUpdate();
+                    if (creeper instanceof EntityBoltCreeper) {
+                        creeper.setDead();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         pierce--;
         if (pierce <= 0) {
             this.setDead();
@@ -52,6 +112,10 @@ public class EntityTakumiArrow extends EntityArrow {
         compound.setInteger("power", this.power);
         compound.setInteger("pierce", this.pierce);
         compound.setBoolean("destroy", this.destroy);
+        if (this.container != null) {
+            compound.setString("container", this.container.getName());
+        }
+        compound.setString("type", this.type.toString());
     }
 
     @Override
@@ -59,10 +123,25 @@ public class EntityTakumiArrow extends EntityArrow {
         this.power = compound.getInteger("power");
         this.pierce = compound.getInteger("pierce");
         this.destroy = compound.getBoolean("destroy");
+        try {
+            this.container = (Class<? extends EntityCreeper>) Class.forName(compound.getString("container"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            this.type = EnumArrowType.valueOf(compound.getString("type"));
+        } catch (Exception e) {
+            this.type = EnumArrowType.NORMAL;
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected ItemStack getArrowStack() {
         return stack;
+    }
+
+    public enum EnumArrowType {
+        NORMAL, PIERCE, MONSTER, SHOT, LASER
     }
 }
--- src/main/java/com/tntmodders/takumi/item/ItemTakumiBowGun.java
+++ src/main/java/com/tntmodders/takumi/item/ItemTakumiBowGun.java
@@ -1,14 +1,15 @@
 package com.tntmodders.takumi.item;
 
 import com.tntmodders.takumi.TakumiCraftCore;
+import com.tntmodders.takumi.block.BlockTakumiMonsterBomb;
+import com.tntmodders.takumi.core.TakumiBlockCore;
 import com.tntmodders.takumi.core.TakumiItemCore;
+import com.tntmodders.takumi.entity.item.EntityTakumiArrow;
+import net.minecraft.block.Block;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityArrow;
-import net.minecraft.item.EnumAction;
-import net.minecraft.item.EnumRarity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
@@ -43,16 +44,36 @@ public class ItemTakumiBowGun extends Item {
             ItemStack itemstack = entityLiving.getHeldItemMainhand();
             ItemStack arrow = entityLiving.getHeldItemOffhand();
             boolean flg = false;
+            EntityArrow takumiArrow = null;
             if (arrow.getItem() == TakumiItemCore.TAKUMI_ARROW_HA) {
-                EntityArrow takumiArrow = TakumiItemCore.TAKUMI_ARROW_HA.createArrow(worldIn, arrow, entityLiving);
+                takumiArrow = TakumiItemCore.TAKUMI_ARROW_HA.createArrow(worldIn, arrow, entityLiving);
                 takumiArrow.setAim(entityLiving, entityLiving.rotationPitch, entityLiving.rotationYaw, 0.0F, 5 * 3.0F,
                         1.0F);
                 takumiArrow.setIsCritical(true);
                 takumiArrow.pickupStatus = EntityArrow.PickupStatus.CREATIVE_ONLY;
                 flg = worldIn.spawnEntity(takumiArrow);
-                entityLiving.knockBack(entityLiving, 1, takumiArrow.motionX, takumiArrow.motionZ);
+            } else if (arrow.getItem() instanceof ItemBlock &&
+                    Block.getBlockFromItem(arrow.getItem()) instanceof BlockTakumiMonsterBomb) {
+                BlockTakumiMonsterBomb bomb = ((BlockTakumiMonsterBomb) Block.getBlockFromItem(arrow.getItem()));
+                takumiArrow =
+                        new EntityTakumiArrow(worldIn, new ItemStack(TakumiItemCore.TAKUMI_ARROW_HA), entityLiving,
+                                bomb.getEntityClass());
+                takumiArrow.setAim(entityLiving, entityLiving.rotationPitch, entityLiving.rotationYaw, 0.0F, 5 * 3.0F,
+                        1.0F);
+                takumiArrow.setIsCritical(true);
+                takumiArrow.pickupStatus = EntityArrow.PickupStatus.CREATIVE_ONLY;
+                flg = worldIn.spawnEntity(takumiArrow);
+            } else if (arrow.getItem() == Item.getItemFromBlock(TakumiBlockCore.CREEPER_BOMB)) {
+                takumiArrow = new EntityTakumiArrow(worldIn, entityLiving, 1, 5, true, null,
+                        EntityTakumiArrow.EnumArrowType.NORMAL);
+                takumiArrow.setAim(entityLiving, entityLiving.rotationPitch, entityLiving.rotationYaw, 0.0F, 5 * 3.0F,
+                        1.0F);
+                takumiArrow.setIsCritical(true);
+                takumiArrow.pickupStatus = EntityArrow.PickupStatus.CREATIVE_ONLY;
+                flg = worldIn.spawnEntity(takumiArrow);
             }
-            if (flg) {
+            if (flg && takumiArrow != null) {
+                entityLiving.knockBack(entityLiving, 1, takumiArrow.motionX, takumiArrow.motionZ);
                 if (!(entityLiving instanceof EntityPlayer && ((EntityPlayer) entityLiving).isCreative())) {
                     arrow.shrink(1);
                     itemstack.damageItem(1, entityLiving);
