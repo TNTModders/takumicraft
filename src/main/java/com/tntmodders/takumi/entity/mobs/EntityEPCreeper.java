07f8878ba926e9551d6dd73e0776c2f36bb4e103
TomKate <tomo23katakata@gmail.com>
2018-07-12 20:11:41 +0900

    [139]爆塵匠の追加

---
+++ src/main/java/com/tntmodders/takumi/entity/mobs/EntityEPCreeper.java
@@ -0,0 +1,70 @@
+package com.tntmodders.takumi.entity.mobs;
+
+import com.tntmodders.takumi.core.TakumiPotionCore;
+import com.tntmodders.takumi.entity.EntityTakumiAbstractCreeper;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.ExplosionEvent.Detonate;
+
+public class EntityEPCreeper extends EntityTakumiAbstractCreeper {
+
+    public EntityEPCreeper(World worldIn) {
+        super(worldIn);
+    }
+
+    @Override
+    public void takumiExplode() {
+        this.world.createExplosion(this, this.posX, this.posY, this.posZ, this.getPowered() ? 6 : 4, true);
+    }
+
+    @Override
+    public EnumTakumiRank takumiRank() {
+        return EnumTakumiRank.LOW;
+    }
+
+    @Override
+    public EnumTakumiType takumiType() {
+        return EnumTakumiType.WIND;
+    }
+
+    @Override
+    public int getExplosionPower() {
+        return 0;
+    }
+
+    @Override
+    public int getSecondaryColor() {
+        return 0x223322;
+    }
+
+    @Override
+    public boolean isCustomSpawn() {
+        return false;
+    }
+
+    @Override
+    public String getRegisterName() {
+        return "epcreeper";
+    }
+
+    @Override
+    public int getRegisterID() {
+        return 60;
+    }
+
+    @Override
+    public boolean takumiExplodeEvent(Detonate event) {
+        event.getAffectedEntities().forEach(entity -> {
+            if (entity instanceof EntityLivingBase) {
+                ((EntityLivingBase) entity).addPotionEffect(new PotionEffect(TakumiPotionCore.EP, 100));
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public int getPrimaryColor() {
+        return 0x111111;
+    }
+}
