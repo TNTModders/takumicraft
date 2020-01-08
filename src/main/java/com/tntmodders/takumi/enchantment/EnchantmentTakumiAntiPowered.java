1e4f8d38933a633477d98065aa3c1f61727599a6
tomkate <tomo23katakata@gmail.com>
2020-01-08 14:09:58 +0900

    エンチャントテーブルで巨匠特攻をつけられるよう修正

--- src/main/java/com/tntmodders/takumi/enchantment/EnchantmentTakumiAntiPowered.java
+++ src/main/java/com/tntmodders/takumi/enchantment/EnchantmentTakumiAntiPowered.java
@@ -1,22 +1,48 @@
 package com.tntmodders.takumi.enchantment;
 
 import com.tntmodders.takumi.TakumiCraftCore;
+import com.tntmodders.takumi.core.TakumiItemCore;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnumEnchantmentType;
-import net.minecraft.init.Items;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
+import java.util.Arrays;
+
 public class EnchantmentTakumiAntiPowered extends Enchantment {
+    private static final Item[] APPLYABLE = {
+            /*Items.DIAMOND_SWORD,*/
+            TakumiItemCore.TAKUMI_SWORD,
+            TakumiItemCore.BATTLE_SHIELD_POWERED,
+            TakumiItemCore.TAKUMI_TYPE_SWORD_FIRE,
+            TakumiItemCore.TAKUMI_TYPE_SWORD_NORMAL,
+            TakumiItemCore.TAKUMI_TYPE_SWORD_WATER,
+            TakumiItemCore.TAKUMI_TYPE_SWORD_GRASS,
+            TakumiItemCore.TAKUMI_TYPE_SWORD_GROUND,
+            TakumiItemCore.TAKUMI_TYPE_SWORD_WIND
+    };
 
     public EnchantmentTakumiAntiPowered() {
-        super(Rarity.VERY_RARE, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND});
+        super(Rarity.RARE, EnumEnchantmentType.WEAPON, new EntityEquipmentSlot[]{EntityEquipmentSlot.MAINHAND});
         this.setRegistryName(TakumiCraftCore.MODID, "takumi_anti_powered");
         this.setName("takumi_anti_powered");
     }
 
     @Override
     public boolean canApplyAtEnchantingTable(ItemStack stack) {
-        return stack.getItem() == Items.DIAMOND_SWORD && super.canApplyAtEnchantingTable(stack);
+        boolean flg = Arrays.asList(APPLYABLE).contains(stack.getItem()) && super.canApplyAtEnchantingTable(stack);
+        TakumiCraftCore.LOGGER.info(flg);
+        return flg;
+    }
+
+    @Override
+    public int getMinEnchantability(int enchantmentLevel) {
+        return 20;
+    }
+
+    @Override
+    public int getMaxEnchantability(int enchantmentLevel) {
+        return 30;
     }
 }
