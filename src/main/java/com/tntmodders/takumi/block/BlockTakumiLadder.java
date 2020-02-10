a440de0d5cdfa27274b8dac24cd89402c7ec208c
tomkate <tomo23katakata@gmail.com>
2020-02-10 22:13:38 +0900

    匠式硬質梯子の硬さ修正

--- src/main/java/com/tntmodders/takumi/block/BlockTakumiLadder.java
+++ src/main/java/com/tntmodders/takumi/block/BlockTakumiLadder.java
@@ -2,6 +2,7 @@ package com.tntmodders.takumi.block;
 
 import com.tntmodders.takumi.TakumiCraftCore;
 import net.minecraft.block.BlockLadder;
+import net.minecraft.block.SoundType;
 
 public class BlockTakumiLadder extends BlockLadder {
 
@@ -10,6 +11,8 @@ public class BlockTakumiLadder extends BlockLadder {
         this.setRegistryName(TakumiCraftCore.MODID, "creeperladder");
         this.setCreativeTab(TakumiCraftCore.TAB_CREEPER);
         this.setUnlocalizedName("creeperladder");
+        this.setHardness(0.4f);
         this.setResistance(10000000f);
+        this.setSoundType(SoundType.LADDER);
     }
 }
